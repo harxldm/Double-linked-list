d2e5c78785f6b0d98362e75dd596933e2ae9d93d
Harold <harold.gutierrez@bjungle.net>
2025-05-11 23:07:03 -0500

    add content of queue and stack

--- src/main/java/SimplementeEnlazada/queue.java
+++ src/main/java/SimplementeEnlazada/queue.java
@@ -4,10 +4,73 @@
  */
 package SimplementeEnlazada;
 
+import java.util.Iterator;
+
 /**
  *
  * @author harxl
  */
-public class queue {
+public class queue<Item> implements Iterable <Item> {
     
+    private Node first;
+    private Node last;
+    private int count;
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+   
+    public void enqueue(Item item){
+        Node oldlast = last;
+        last = new Node() ;
+        last.item = item;
+        oldlast.next = last;
+        count++;
+        if(isEmpty())
+            first = last;
+        count++;
+    }
+    
+    public Item dequeue(){
+        Item item = first.item;
+        first.item = null;
+        first = first.next ;
+        count--;
+        if(isEmpty())
+            last = null;
+        return item;
+        
+    }
+    
+    public boolean isEmpty(){
+        return first == null;
+        //return count = 0;
+    }
+    
+    public int size(){
+        return count ;
+    }
+    
+        @Override
+     public Iterator<Item> iterator() {
+        return new LinkedListIterator ();  
+     }
+    private class LinkedListIterator implements Iterator<Item>{
+        
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+             return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        
+    }
 }
--- src/main/java/SimplementeEnlazada/stack.java
+++ src/main/java/SimplementeEnlazada/stack.java
@@ -8,6 +8,76 @@ package SimplementeEnlazada;
  *
  * @author harxl
  */
-public class stack {
+import java.util.Iterator;
+
+/**
+ *
+ * @author sala5
+ */
+public class stack<Item> implements Iterable<Item> {
+    private Node first;
+    private int count;
+    
+    public stack() {
+        first = null;
+        count = 0;
+    }
+
+    
+    private class Node {
+        Item item;
+        Node next;
+    }
+    
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        count++;
+    }
+    
+    public Item pop() {
+        Item item = first.item;
+        first.item = null;
+        first = first.next;
+        count--;
+        return item;
+    }
+    
+    public Item peak() {
+        return first.item;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public int size() {
+        return count;
+    }
+    
+    @Override
+    public Iterator<Item> iterator() {
+        return new LinkedListIterator();
+    }
+    
+    private class LinkedListIterator implements Iterator<Item> {
+        
+        private Node current = first;
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        
+    }
     
 }
+
