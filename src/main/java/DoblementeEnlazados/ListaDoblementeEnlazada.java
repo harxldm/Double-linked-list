3ac73dafc6cd7c31d90ebc164496c70e27a0fd91
Harold <harold.gutierrez@bjungle.net>
2025-05-11 23:27:28 -0500

    Add methods addLast and addFirst

--- src/main/java/DoblementeEnlazados/ListaDoblementeEnlazada.java
+++ src/main/java/DoblementeEnlazados/ListaDoblementeEnlazada.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DoblementeEnlazados;
+import java.util.Iterator;
 
 /**
  *
  * @author harxl
  */
-public class ListaDoblementeEnlazada {
+public class ListaDoblementeEnlazada<Item> implements Iterable{
+    Node first;
+    Node last;
+    int count;
+
     
+    private class Node {
+    Item item;
+    Node next;
+    Node preview;
+        
+    }
+    
+    public void addLast(Item item) {
+        Node oldLast = last;
+        Node last = new Node();
+        last.item = item;
+        last.preview = oldLast;
+    }
+    
+    public void addFirst(Item item) {
+        Node oldFirst = first;
+        Node first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+    
+    public void add(Item item) {
+        
+    }
+    
+    
+          
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
