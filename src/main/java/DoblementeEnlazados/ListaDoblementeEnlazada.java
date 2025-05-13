4c60a29f60daffee2077b4d315ea680c7853a650
Harold <harold.gutierrez@bjungle.net>
2025-05-13 00:33:35 -0500

    Complete all linkedList

--- src/main/java/DoblementeEnlazados/ListaDoblementeEnlazada.java
+++ src/main/java/DoblementeEnlazados/ListaDoblementeEnlazada.java
@@ -27,6 +27,7 @@ public class ListaDoblementeEnlazada<Item> implements Iterable{
         Node last = new Node();
         last.item = item;
         last.preview = oldLast;
+        count++;
     }
     
     public void addFirst(Item item) {
@@ -34,16 +35,98 @@ public class ListaDoblementeEnlazada<Item> implements Iterable{
         Node first = new Node();
         first.item = item;
         first.next = oldFirst;
+        count++;
     }
     
-    public void add(Item item) {
+    public Item remove() {
+        Item item = last.item;
+        last.item = null;
+        last = last.preview ;
+        count--;
+        if(isEmpty())
+            last = null;
+        return item;
+    }
+    
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    
+    public int size() {
+        return count ;
+    }
+    
+    public void add(int i, Item item) {
+       int cont = 0;
+        for (Node x = first; x != null; x = x.next) {
+            if (cont == i) {
+                Node prev = x.preview;
+                Node newNode = new Node();
+                newNode.item = item;
+                prev.next = newNode;
+                x.preview = newNode;
+                newNode.next = x;
+                count++;
+            } else {
+                cont++;
+            }
+        }
+    }
+    
+    public void set(int i, Item item) {
+        int cont = 0;
+        for (Node x = first; x != null; x = x.next) {
+            if (cont == i) {
+                x.item = item;
+                count++;
+            } else {
+                cont++;
+            }
+        }
+    }
+    
+    public void removeByElement(Item item) {
+        for (Node x = first; x != null; x = x.next) {
+            if (x.item.equals(item)) {
+                Node prev = x.preview;
+                Node next = x.next;
+                
+                if (prev != null) {
+                prev.next = next;
+            } else {
+                first = next;
+            }
+
+            if (next != null) {
+                next.preview = prev;
+            } else {
+                last = prev;
+            }
+                count--;
+                break;
+            }
+        }
+    }
+    
+      @Override
+     public Iterator<Item> iterator() {
+        return new LinkedListIterator ();  
+     }
+    private class LinkedListIterator implements Iterator<Item>{
         
-    }
-    
-    
-          
-    @Override
-    public Iterator iterator() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+             return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        
     }
 }
